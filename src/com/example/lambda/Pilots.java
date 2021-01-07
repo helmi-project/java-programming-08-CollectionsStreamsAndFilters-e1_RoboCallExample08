d8fa42dc99785dcea8651ffd4a29b1561080c3dc
Helmi Hartanto <>
2021-01-07 09:41:44 +0700

    First Commit

---
+++ src/com/example/lambda/Pilots.java
@@ -0,0 +1,11 @@
+package com.example.lambda;
+
+import java.util.function.Predicate;
+
+public class Pilots implements Predicate<Person>{
+    
+    @Override
+    public boolean test(Person p){
+        return p.getAge() >= 23 && p.getAge() <= 65;
+    }
+}
